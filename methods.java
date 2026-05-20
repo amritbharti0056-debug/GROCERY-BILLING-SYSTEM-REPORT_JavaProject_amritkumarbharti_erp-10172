8d097d46bde83b5b370b9bee67ace895a36f55bb
amritbharti0056-debug <amritbharti0056@gmail.com>
2026-05-20 11:26:44 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,9 @@
+public class methods {
+    public static void main(String[] args) {
+        System.out.println("Area of circle with radius 5: " + areaOfCircle(5));
+    }
+    //area of circle
+    public static double areaOfCircle(double radius) {
+        return Math.PI * radius * radius;
+    }
+} 
